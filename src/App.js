0d29a5f34baad9bce6a73a539ace02cc1b94d552
Rudy Gagneron <rgagneron@xcalar.com>
2020-12-06 14:00:00 -0800

    add search input

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './styles.scss';
 
 function App() {
     const [allPokemon, setAllPokemon] = useState([]);
+    const [filteredPokemonList, setFilteredPokemonList] = useState([]);
+    const [searchValue, setSearchValue] = useState("");
 
     useEffect(() => {
         fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
@@ -11,6 +13,7 @@ function App() {
         .then(res => {
             console.log(res);
             setAllPokemon(res.results);
+            setFilteredPokemonList(res.results);
             // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
         })
         .catch(() => {
@@ -25,10 +28,30 @@ function App() {
         return index;
     }
 
+    const updateSearchValue = (searchVal) => {
+        setSearchValue(searchVal);
+        // TODO: use setTimeout to wait for user to finish typing
+        getFilteredPokemonList(searchVal);
+    }
+
+    const getFilteredPokemonList = (searchVal) => {
+        if (!searchValue) {
+            return allPokemon;
+        }
+        searchVal = searchVal.toLowerCase();
+        setFilteredPokemonList(allPokemon.filter((pokemon) => {
+            // TODO: consider showing matches that contain rather than start with
+            return pokemon.name.toLowerCase().startsWith(searchVal);
+        }));
+    }
+
     return (
         <div className="App">
             <div className="flexContainer">
-            {allPokemon.map((pokemon) => {
+                <input placeholder="search" type="search" value={searchValue} onChange={(e) => {updateSearchValue(e.target.value)}} />
+            </div>
+            <div className="flexContainer">
+            {filteredPokemonList.map((pokemon) => {
                 let id = getPokeId(pokemon.url);
                 let imgSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
                 return <div key={id} className="card">
@@ -42,3 +65,4 @@ function App() {
 }
 
 export default App;
+
