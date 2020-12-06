019882b253f4265f6e33846257433f0532a50c0f
Rudy Gagneron <rgagneron@xcalar.com>
2020-12-06 13:51:15 -0800

    first commit - display 151 pokemon images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import './styles.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [allPokemon, setAllPokemon] = useState([]);
+
+    useEffect(() => {
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+            setAllPokemon(res.results);
+            // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+        })
+        .catch(() => {
+            alert("failed to get pokemon list");
+        });
+    }, []);
+
+    // split url value of /pokemon api to get the id
+    const getPokeId = (pokeUrl) => {
+        let split = pokeUrl.split("/");
+        let index = split[split.length - 2];
+        return index;
+    }
+
+    return (
+        <div className="App">
+            <div className="flexContainer">
+            {allPokemon.map((pokemon) => {
+                let id = getPokeId(pokemon.url);
+                let imgSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+                return <div key={id} className="card">
+                        <img className="cardImg" src={imgSrc} />
+                        <div className="label">{pokemon.name}</div>
+                </div>
+            })}
+            </div>
+        </div>
+    );
 }
 
 export default App;
